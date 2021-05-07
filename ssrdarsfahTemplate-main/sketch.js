aae96894c2d44ba09d173f5eb725fd5ed1e7b092
avianshavasthi4 <77564039+avianshavasthi4@users.noreply.github.com>
2021-05-07 19:57:12 +0530

    Add files via upload

---
+++ ssrdarsfahTemplate-main/sketch.js
@@ -0,0 +1,104 @@
+var canvas;
+var block1,block2,block3,block4;
+var ball, edges;
+var music;
+var test = 0;
+function preload(){
+    // load sound here
+    music = loadSound("Spring-Boing.mp3")
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    block1 = createSprite(0,580,360,30);
+    block1.shapeColor = "blue";
+
+    block2 = createSprite(285,580,200,30);
+    block2.shapeColor = "orange";
+
+    block3 = createSprite(500,580,200,30);
+    block3.shapeColor = "red";
+
+    block4 = createSprite(720,580,200,30);
+    block4.shapeColor = "green";
+
+    
+
+    ball = createSprite(random(20,750),100, 40,40);
+    ball.shapeColor = rgb(255,255,255);
+    //write code to add velocityX and velocityY
+    ball.velocityX = 10
+    ball.velocityY = 10
+}
+
+function draw() {
+    background("grey");
+    edges=createEdgeSprites();
+
+    if(ball.isTouching(block1)){
+        ball.shapeColor = "blue";
+        music.play();
+    }
+
+
+    if(ball.isTouching(block2)){
+        ball.shapeColor = "orange";
+        ball.velocityX = 0;
+        ball.velocityY = 0;
+
+        
+        
+    }
+
+
+    if(ball.isTouching(block3)){
+        ball.shapeColor = "red";
+    music.play();
+    }
+
+    if(ball.isTouching(block4)){
+        ball.shapeColor = "green";
+        music.play();
+    }
+
+    ball.bounceOff(edges);
+    ball.bounceOff(block1);
+    ball.bounceOff(block2);
+    ball.bounceOff(block3);
+    ball.bounceOff(block4);
+
+
+    
+    //write code to bounce off ball from the block1 
+    
+        //write code to set velocityX and velocityY of ball as 0
+
+        //write code to stop music
+    
+
+    //write code to bounce off ball from the block3
+
+
+    //write code to bounce off ball from the block4
+
+    drawSprites();
+    displayText();
+
+
+}
+
+
+function displayText(){
+    if(ball.velocityX === 0){
+
+
+        textSize(15);
+        stroke("white"); 
+        text("KYA GUNDA BANEGA RE TU",350,300);
+        
+        
+    }
+
+}
